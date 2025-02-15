d2f79c34bffa5cb2f26768a48efb156130bbbc66
Kyra Brown <kybrown@wpi.edu>
2025-02-14 14:00:33 -1000

    made the home page look better

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,16 +8,21 @@ const Home = () => {
         Join our initiative to restore native Hawaiian plant life through rain gardens.
         Learn more about how you can participate and track existing gardens.
       </p>
-      <a href="https://www.protectpreservehi.org/general-6-1#/ar-gardens" className="App-link">
-        Learn More
-      </a>
-      <div className="mt-8 flex space-x-4">
-        <button className="px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition">
-          Existing Gardener Sign In
-        </button>
-        <button className="px-6 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition">
-          Admin Sign In
-        </button>
+    
+
+
+        <div className="button-container">
+        {/* Learn More Button */}
+        <a href="https://www.protectpreservehi.org/general-6-1#/ar-gardens" className="btn btn-blue">
+            Learn More
+        </a>
+        <div className="mt-8 flex space-x-4">
+            {/* Sign In */}
+            <button className="btn btn-blue">
+            Sign In
+            </button>
+        </div>
+
       </div>
     </div>
   );
