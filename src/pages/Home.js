3fbd11185909774e9526681aff947efb0b6b9aaf
Kyra Brown <kybrown@wpi.edu>
2025-02-17 10:53:12 -1000

    added admin and updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,22 @@
 import "../App.css"; // Ensure your CSS is imported
 
+// import { useRouter } from 'next/navigation'; // Import useRouter for navigation
+
 const Home = () => {
+
+  // const router = useRouter(); // initialize router
+
   return (
     <div className="App-header">
       <h1>Protect & Preserve Hawaiʻi</h1>
       <p>
-        Join our initiative to restore native Hawaiian plant life through rain gardens.
+        Join our initiative to restore native Hawaiian plant life and control stormwater runoff through rain gardens!
         Learn more about how you can participate and track existing gardens.
       </p>
-    
 
+      <p>
+        Learn more about how you can participate and track existing gardens.
+      </p>
 
         <div className="button-container">
         {/* Learn More Button */}
@@ -19,6 +26,7 @@ const Home = () => {
         <div className="mt-8 flex space-x-4">
             {/* Sign In */}
             <button className="btn btn-blue">
+            {/* <button className="btn btn-blue" onClick={() => router.push('/Admin')}> */}
             Sign In
             </button>
         </div>
