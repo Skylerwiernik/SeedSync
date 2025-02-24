39ce2c8b13d79f1c6cf0a296288ad00f305cadbf
Skyler Wiernik <skyler@Skylers-MacBook-Air.local>
2025-02-24 11:13:01 -1000

    Removed unused perm code

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,56 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {doc, getDoc, query, collection, orderBy, limit, startAt, endAt, getFirestore, getDocs} from "firebase/firestore";
-import { getAuth, onAuthStateChanged } from "firebase/auth"; // Import authentication
+import {query, collection, orderBy, limit, startAt, endAt, getFirestore, getDocs} from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // Import authentication
 import "../App.css";
 import {app} from "../firebase";
 
 const db = getFirestore(app);
-const auth = getAuth(app); // to check if user is admin or not
+const auth = getAuth(app);
 
 const Admin = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState([]);
   const navigate = useNavigate();
 
-  // loading 
-  const [loading, setLoading] = useState(true); // default loading set to true
-  
-  // create react states for checking if it IS an admin
-  const [isAdmin, setIsAdmin] = useState(null);
-
-  // useEffect(() => { // checks if an admin or not and sets react var
-  //   const checkAdminStatus = async (user) => {
-  //     if (user) {
-  //       const userDocRef = doc(db, "users", user.uid);
-  //       const userSnap = await getDoc(userDocRef);
-
-  //       if (userSnap.exists() && userSnap.data().role === "admin") {
-  //         setIsAdmin(true);
-  //       } else {
-  //         setIsAdmin(false);
-  //       }
-  //     } else {
-  //       setIsAdmin(false);
-  //     }
-  //     setLoading(false); // no longer loading
-  //   }
-
-  //   const unsubscribe = onAuthStateChanged(auth, (user) => {
-  //     checkAdminStatus(user);
-  //   });
-
-  //   return () => unsubscribe();
-  // })
-
     // calls provided search function whenever input changes
     const handleSearch = async (e) => {
       const value = e.target.value;
       setSearchTerm(value);
-
-      if (!isAdmin) {
-        alert("You do not have permission to search."); // alert for non-admins
-      }
 
       if (value.trim() !== "") {
         const data = await queryFirebase(value);
@@ -101,15 +67,10 @@ const Admin = () => {
     return Object.values(matches);
   }
 
-  // if (loading) {
-  //   return <p>Loading...</p>; // show loaidng state while chekding admin status
-  // }
-
   return (
     <div className="admin-container">
       <h1 className="subtitle">Admin Dashboard</h1>
 
-      {/* {!isAdmin && <p className="error-message">🚫 You do not have permission to search.</p>} */}
 
       <div className="search-container">
         <input
@@ -118,7 +79,6 @@ const Admin = () => {
           value={searchTerm}
           onChange={handleSearch}
           className="search-bar"
-          // disabled={!isAdmin} // disable input for non-admins
         />
       </div>
 
